62f5dfbfcde5c51a345031c2efd58f155d5d1504
Nicholas Angeles <blackdeath546@gmail.com>
2024-10-03 18:18:37 -0400

    Bank Heist add-in

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bankHeist = "Bank Heist";
+
+            string currency = "Dollars";
+            
+            int teamMembers = 4;
+            
+            char vaultNumber = 'B';
+            
+            bool isBankOpen = false;
+
+            double goalHeistAmount = 1000000.00;
+            
+            decimal actualBankHeistAmount = 500.00m;
+            
+            Console.WriteLine($"Today we are planning for a bank heist with {teamMembers} teammembers" +
+                              $" to obtain {goalHeistAmount} {currency}." +
+                              $"\nUnfortunately we only obtained {actualBankHeistAmount} {currency} from " +
+                              $"{vaultNumber}." +
+                              $"\nThe bank's open status is {isBankOpen}. How did we miss that!");
         }
     }
 }
